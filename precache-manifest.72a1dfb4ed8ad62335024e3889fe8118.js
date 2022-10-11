b311aba63f1b534de99628274207ec1ba1c37e25
Pardeep <pardeep@topdevit.com>
2022-10-11 17:44:33 +0530

    Initial Commet

---
+++ precache-manifest.72a1dfb4ed8ad62335024e3889fe8118.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = [
+  {
+    "revision": "d233fa14ceed93a4d869",
+    "url": "/myfirstlaravelvue/css/app.6713a8e4.css"
+  },
+  {
+    "revision": "d233fa14ceed93a4d869",
+    "url": "/myfirstlaravelvue/js/app.606f5728.js"
+  },
+  {
+    "revision": "45c1c907d410ad2ed150",
+    "url": "/myfirstlaravelvue/css/auth.ff668b45.css"
+  },
+  {
+    "revision": "45c1c907d410ad2ed150",
+    "url": "/myfirstlaravelvue/js/auth.a19294bc.js"
+  },
+  {
+    "revision": "27c2f7d725038f1e1d82",
+    "url": "/myfirstlaravelvue/js/charts~dashboard.5eeae7ba.js"
+  },
+  {
+    "revision": "622a910aacdf43d58f5a",
+    "url": "/myfirstlaravelvue/js/chunk-9c23e20e.7f9c6d41.js"
+  },
+  {
+    "revision": "4f8abbed9400450eea5d",
+    "url": "/myfirstlaravelvue/css/chunk-b66bf348.036e2385.css"
+  },
+  {
+    "revision": "4f8abbed9400450eea5d",
+    "url": "/myfirstlaravelvue/js/chunk-b66bf348.79654379.js"
+  },
+  {
+    "revision": "1bbbaf29ed49fad40db0",
+    "url": "/myfirstlaravelvue/css/maps.9cd28c16.css"
+  },
+  {
+    "revision": "1bbbaf29ed49fad40db0",
+    "url": "/myfirstlaravelvue/js/maps.9f325938.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/myfirstlaravelvue/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/myfirstlaravelvue/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/myfirstlaravelvue/fonts/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/myfirstlaravelvue/fonts/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/myfirstlaravelvue/fonts/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/myfirstlaravelvue/fonts/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "7f906302c4f85bdd968856f1377b7ae5",
+    "url": "/myfirstlaravelvue/index.html"
+  },
+  {
+    "revision": "702fb8c567cd92b4f3c242abc84a8b56",
+    "url": "/myfirstlaravelvue/img/AU.png"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/myfirstlaravelvue/favicon.png"
+  },
+  {
+    "revision": "f8624a716c4efc245ad1550c657b5f98",
+    "url": "/myfirstlaravelvue/img/apple-icon.png"
+  },
+  {
+    "revision": "ce0474dc19b6bb2dcfb27a489754a859",
+    "url": "/myfirstlaravelvue/img/balmain.jpg"
+  },
+  {
+    "revision": "98b6b57a2e3ee1baf1b9ac76f9a04e09",
+    "url": "/myfirstlaravelvue/img/bg1.jpg"
+  },
+  {
+    "revision": "0d1e6e2a7d2a685cef8dc8a5fbbe6cac",
+    "url": "/myfirstlaravelvue/img/bg13.jpg"
+  },
+  {
+    "revision": "53e627ca2acad7eee63a70e27c7792d7",
+    "url": "/myfirstlaravelvue/img/bg14.jpg"
+  },
+  {
+    "revision": "f5ccf1a207d9082cc6b6e0769b5c7efa",
+    "url": "/myfirstlaravelvue/img/bg15.jpg"
+  },
+  {
+    "revision": "0a929a3f8a55e7a094af71b3b22033af",
+    "url": "/myfirstlaravelvue/img/BR.png"
+  },
+  {
+    "revision": "cfd464b36416dab7fcdcc09e009a3819",
+    "url": "/myfirstlaravelvue/img/bg16.jpg"
+  },
+  {
+    "revision": "64ababf7de758dcee0358c2c4d93a371",
+    "url": "/myfirstlaravelvue/img/DE.png"
+  },
+  {
+    "revision": "f77d0df953adcfedc2cab68651a1d01b",
+    "url": "/myfirstlaravelvue/img/bg3.jpg"
+  },
+  {
+    "revision": "b0d54b4089ce5e24c82574109f346289",
+    "url": "/myfirstlaravelvue/img/GB.png"
+  },
+  {
+    "revision": "f68d9108445df3450b1eed7ae0256fc1",
+    "url": "/myfirstlaravelvue/img/bg5.jpg"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/myfirstlaravelvue/img/default-avatar.png"
+  },
+  {
+    "revision": "a30e21b28cb8bfddda9d33d5a5e4d28b",
+    "url": "/myfirstlaravelvue/img/emilyz.jpg"
+  },
+  {
+    "revision": "501ba29ef8fa01dc0d2a224508ac59d5",
+    "url": "/myfirstlaravelvue/img/icon-vue.png"
+  },
+  {
+    "revision": "7b8a6099ce9bdadcf1bcfde26a139275",
+    "url": "/myfirstlaravelvue/img/favicon.png"
+  },
+  {
+    "revision": "49458d8716dcfc74c2e95064dbb125d2",
+    "url": "/myfirstlaravelvue/img/header.jpg"
+  },
+  {
+    "revision": "16f109f847c0133b454f5598e55827ce",
+    "url": "/myfirstlaravelvue/img/image_placeholder.jpg"
+  },
+  {
+    "revision": "8c8f7d2b790a7c7f399375155c3cffbe",
+    "url": "/myfirstlaravelvue/img/james.jpg"
+  },
+  {
+    "revision": "a6178f443133606fe19006604d0cafb5",
+    "url": "/myfirstlaravelvue/img/loading.gif"
+  },
+  {
+    "revision": "54c7e270d82bafb7a5ae359aa1d318a3",
+    "url": "/myfirstlaravelvue/img/RO.png"
+  },
+  {
+    "revision": "4ec805b26cd469c628ed056227ff3df1",
+    "url": "/myfirstlaravelvue/img/mike.jpg"
+  },
+  {
+    "revision": "7e82584da3add793adab40d93e519179",
+    "url": "/myfirstlaravelvue/img/now-logo.png"
+  },
+  {
+    "revision": "2f88ea36fc94779a8f2e65c2ee8ccc76",
+    "url": "/myfirstlaravelvue/img/US.png"
+  },
+  {
+    "revision": "54a7dcaf03d55cc8050e81d4569bd05c",
+    "url": "/myfirstlaravelvue/img/placeholder.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/myfirstlaravelvue/robots.txt"
+  },
+  {
+    "revision": "8abc72640ffcac89f657378be61c72c7",
+    "url": "/myfirstlaravelvue/img/prada.jpg"
+  },
+  {
+    "revision": "7465ad2c65edd67cbee2809547bfe4df",
+    "url": "/myfirstlaravelvue/img/saint-laurent.jpg"
+  }
+];
